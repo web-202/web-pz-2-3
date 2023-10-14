38ffa7a384f4e2441691ffbe0592efa6050af391
Maksymchukkk <ivanmaksumchuk2021@gmail.com>
2023-10-14 22:47:20 +0300

    completed task2 maksymchuk_203

--- task_2/index.js
+++ task_2/index.js
@@ -1,9 +1,17 @@
 const button = document.getElementById("status-button");
-  const states = ["pending", "success"];
-  let currentState = 0;
 
-  button.addEventListener("click", () => {
-    button.classList.remove(states[currentState]);
-    currentState = (currentState + 1) % states.length;
-    button.classList.add(states[currentState]);
-  });
+const state = {
+  0: { className: "button-update", textContent: "update" },
+  1: { className: "button-delete", textContent: "delete" },
+  2: { className: "button-pendding", textContent: "pendding" },
+};
+
+let buttonState = 0;
+
+button.addEventListener("click", () => {
+  buttonState = (buttonState + 1) % 3;
+  const { className, textContent } = state[buttonState];
+  button.className = className;
+  button.textContent = textContent;
+});
+
