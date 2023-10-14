cfd93529ba2ffd1e6c6d9bbe2174b51c722bda88
D.Kozlov <novolan2014@gmail.com>
2023-10-14 21:09:29 +0300

    fix task_1 source image label

--- task_1/index.js
+++ task_1/index.js
@@ -4,6 +4,8 @@ const image_3 = document.getElementById('img_3');
 const loginForm = document.getElementById("form");
 const demo = document.getElementById('demo')
 
+
+
     loginForm.addEventListener('submit', (e) => {
         e.preventDefault()
         const name = document.getElementById('name')
@@ -28,7 +30,7 @@ const demo = document.getElementById('demo')
             last.style.borderColor = 'black'
         }
 
-        if(isNaN(age.value) || Number(age.value) < 0){
+        if(isNaN(age.value) || Number(age.value) < 0 || age.value == ""){
             age.style.borderColor = 'red'
             check = false
         }else{
@@ -40,36 +42,6 @@ const demo = document.getElementById('demo')
         }else{
             demo.innerText = 'Not valid'
         }
-    })
-
-    image_1.addEventListener('mouseover', () => {
-        const text = document.getElementById('text_1')
-        text.value=image_1.getAttribute('src')
-    })
-
-    image_1.addEventListener('mouseout', () => {
-        const text = document.getElementById('text_1')
-        text.value=""
-    })
-
-    image_2.addEventListener('mouseover', () => {
-        const text = document.getElementById('text_2')
-        text.value=image_2.getAttribute('src')
-    })
-    
-    image_2.addEventListener('mouseout', () => {
-        const text = document.getElementById('text_2')
-        text.value=""
-    })
-
-    image_3.addEventListener('mouseover', () => {
-        const text = document.getElementById('text_3')
-        text.value=image_3.getAttribute('src')
-    })
-
-    image_3.addEventListener('mouseout', () => {
-        const text = document.getElementById('text_3')
-        text.value=""
     })
 
     image_1.addEventListener('click', () => {
@@ -89,17 +61,66 @@ const demo = document.getElementById('demo')
     let rotate_img_2 = 0
     let rotate_img_3 = 0
 
-    image_1.addEventListener('contextmenu', () => {
+    image_1.addEventListener('contextmenu', (e) => {
+        e.preventDefault()
         rotate_img_1 += 90
         image_1.style.transform = `rotate(${rotate_img_1}deg)`
     })
 
-    image_2.addEventListener('contextmenu', () => {
+    image_2.addEventListener('contextmenu', (e) => {
+        e.preventDefault()
         rotate_img_2 += 90
         image_2.style.transform = `rotate(${rotate_img_2}deg)`
     })
 
-    image_3.addEventListener('contextmenu', () => {
+    image_3.addEventListener('contextmenu', (e) => {
+        e.preventDefault()  
         rotate_img_3 += 90
         image_3.style.transform = `rotate(${rotate_img_3}deg)`
     })
+
+    const img_text_1 = document.getElementById('img_text_1')
+    const img_text_2 = document.getElementById('img_text_2')
+    const img_text_3 = document.getElementById('img_text_3')
+
+    const valueImglName = () => {
+        const image_name_1 = image_1.getAttribute('src').split('/')[3].split('.')[0]
+        const image_name_2 = image_2.getAttribute('src').split('/')[3].split('.')[0]
+        const image_name_3 = image_3.getAttribute('src').split('/')[3].split('.')[0]
+
+        img_text_1.textContent = image_name_1
+        img_text_2.textContent = image_name_2
+        img_text_3.textContent = image_name_3
+    }
+    
+img_text_1.addEventListener('mouseover', (e) => {
+    e.preventDefault()
+    img_text_1.innerText = img_text_1.textContent + `(${image_1.getAttribute('src')})`
+})
+
+img_text_1.addEventListener('mouseout', () => {
+    img_text_1.innerText = image_1.getAttribute('src').split('/')[3].split('.')[0]
+})
+
+img_text_2.addEventListener('mouseover', (e) => {
+    e.preventDefault()
+    img_text_2.innerText = img_text_2.textContent + `(${image_2.getAttribute('src')})`
+})
+
+img_text_2.addEventListener('mouseout', () => {
+    img_text_2.innerText = image_2.getAttribute('src').split('/')[3].split('.')[0]
+})
+
+
+img_text_3.addEventListener('mouseover', (e) => {
+    e.preventDefault()
+    img_text_3.innerText = img_text_3.textContent + `(${image_3.getAttribute('src')})`
+})
+
+img_text_3.addEventListener('mouseout', () => {
+    img_text_3.innerText = image_3.getAttribute('src').split('/')[3].split('.')[0]
+})
+
+
+
+valueImglName()
