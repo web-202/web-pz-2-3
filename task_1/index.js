a2e3891ee5e8eb01cf66855488e7d497c5aceec5
Maksymchukkk <ivanmaksumchuk2021@gmail.com>
2023-10-17 16:08:43 +0300

    fix pz-2-3 maksymchuk_203

--- task_1/index.js
+++ task_1/index.js
@@ -1,29 +1,34 @@
-function alertt(img) {
-    alert('Ви переглядаєте ' + img.alt);
-  }
+    
+    function alertt(img) {
+        alert('Ви переглядаєте ' + img.alt);
+    }
+    const img = document.getElementById('yourImageId'); 
+  
+    function rotateImage(img) {
+        const currentRotation = img.getAttribute('data-rotation') || 0;
+        const newRotation = (parseInt(currentRotation) + 90) % 360;
 
-  function rotateImage(img) {
-    const currentRotation = img.getAttribute('data-rotation') || 0;
-    const newRotation = (parseInt(currentRotation) + 90) % 360;
+        img.style.transform = `rotate(${newRotation}deg)`;
+        img.setAttribute('data-rotation', newRotation);
+        img.style.marginTop = newRotation % 180 === 90 ? '40px' : '0';
+    }
 
-    img.style.transform = `rotate(${newRotation}deg)`;
-    img.setAttribute('data-rotation', newRotation);
-    img.style.marginTop = newRotation % 180 === 90 ? '40px' : '0';s
-  }
+    const userForm = document.getElementById('add-form');
 
-  userForm.addEventListener("submit", function (e) {
-    e.preventDefault(); 
+    userForm.addEventListener('submit', function (e) {
+    e.preventDefault();
 
-    const firstNameInput = userForm.elements.firstName;
-    const lastNameInput = userForm.elements.lastName;
-    const ageInput = userForm.elements.age;
+    const firstNameInput = document.getElementById('input_name');
+    const lastNameInput = document.getElementById('input_last_name');
+    const ageInput = document.getElementById('input_age');
+    const demoBlock = document.getElementById('demo');
 
     const firstName = firstNameInput.value.trim();
     const lastName = lastNameInput.value.trim();
     const age = ageInput.value.trim();
 
-    const firstNameRegex = /^[A-Za-z\s]{1,50}$/;
-    const lastNameRegex = /^[A-Za-z\s]{1,50}$/;
+    const firstNameRegex = /^[A-Za-z ]{1,50}$/;
+    const lastNameRegex = /^[A-Za-z ]{1,50}$/;
     const ageRegex = /^\d+$/;
 
     const firstNameValid = firstName.match(firstNameRegex);
@@ -35,19 +40,35 @@ function alertt(img) {
     ageInput.classList.remove("invalid");
 
     if (firstNameValid && lastNameValid && ageValid) {
-        demoBlock.innerHTML = `first name: ${firstName}<br>last: ${lastName}<br>age: ${age}`;
+        demoBlock.innerHTML = `Форма валідна. ${firstName}  ${lastName}, ${age}y.o`;
     } else {
         demoBlock.innerHTML = "Не вірно введені дані";
-
+        
         if (!firstNameValid) {
             firstNameInput.classList.add("invalid");
+            firstNameInput.style.borderColor = "red";
         }
+        if (firstNameValid) {
+            firstNameInput.classList.remove("invalid");
+            firstNameInput.style.borderColor = "";
+        }
+
         if (!lastNameValid) {
             lastNameInput.classList.add("invalid");
+            lastNameInput.style.borderColor = "red";
         }
+        if (lastNameValid) {
+            lastNameInput.classList.remove("invalid");
+            lastNameInput.style.borderColor = "";
+        }
+
         if (!ageValid || parseInt(age) < 0) {
             ageInput.classList.add("invalid");
+            ageInput.style.borderColor = "red";
+        }
+        if (ageValid) {
+            ageInput.classList.remove("invalid");
+            ageInput.style.borderColor = "";
         }
     }
-});
-
+});   
