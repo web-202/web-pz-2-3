023ac3a9096d626a9049f94bbda149121df4fac1
Denis_Kozub <kozubenkodennis@gmail.com>
2023-10-17 15:33:57 +0300

    fix

--- task_2/index.js
+++ task_2/index.js
@@ -1,8 +1,15 @@
 const button = document.getElementById("statusButton");
 const statuse = ["grean", "yelow", "red"];
+const texts = ["GREEN", "YELLOW", "RED"];
 let currentStatusIndex = 0;
 
 button.addEventListener("click", () => {
   button.className = `button ${statuse[currentStatusIndex]}`;
+  button.textContent = texts[currentStatusIndex];
   currentStatusIndex = (currentStatusIndex + 1) % statuse.length;
 });
+
+
+
+
+
