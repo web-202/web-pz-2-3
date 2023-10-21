dedfe18fa86c134a9c48be054b918251b0761c0c
OleksandrIX <zagorenkosasha@gamil.com>
2023-10-21 14:56:22 +0300

    (fix) fix task 2

--- task_2/index.js
+++ task_2/index.js
@@ -3,10 +3,13 @@ const btn = document.querySelector(".page-app .button");
 const swithcedBtnType = (currentBtnType) => {
     switch (currentBtnType) {
         case "button_update":
+            btn.textContent = "Delete";
             return "button_delete";
         case "button_delete":
+            btn.textContent = "Pendding";
             return "button_pendding";
         case "button_pendding":
+            btn.textContent = "Update";
             return "button_update"
         default:
             alert("There is no such button");
