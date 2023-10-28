a031520bb11f04f508a80371594326bb408c1228
Batman190803 <svatik.bs@gmail.com>
2023-10-28 13:37:03 +0300

    complete task

--- script/sctript.js
+++ script/sctript.js
@@ -7,10 +7,12 @@ document.querySelectorAll('#task1').forEach(item => {
 // /Task 1
 
 // Task 2
+let deg = 90
 const title = document.querySelector('#titleTask2')
 document.querySelectorAll('#itemTask2').forEach(item => {
-    item.addEventListener('mouseover', () => {
-        title.textContent = item.textContent
+    item.addEventListener('click', () => {
+        item.style.transform = `rotate(${deg}deg)`
+        deg += 90
     })
 });
 // /Task 2
@@ -27,78 +29,70 @@ document.querySelectorAll('#itemTask3').forEach(item => {
 });
 // /Task 3
 
-
 // Task 4
-document.querySelectorAll('#itemTask4').forEach(item => {
-    const defaultValue = item.textContent
-    let countMove = 0
-    item.addEventListener('mouseover', () => {
-        item.textContent += `\n(${item.href})`
-    }, { once: true })
-    item.addEventListener('mouseout', () => {
-        item.textContent = defaultValue
-    })
-});
+const validateForm =()=>{
+    const firstName = document.getElementById('firstName');
+    const lastName = document.getElementById('lastName');
+    const age = document.getElementById('age');
+        
+    console.log('+');
+    const nameRegex = /^[a-zA-Z]+$/
+    const ageRegex = /^\d+$/
+    console.log(nameRegex.test(firstName.value));
+    if (!nameRegex.test(firstName.value)) {
+        document.getElementById('firstName').classList.add('invalid');
+        return
+      } else {
+        document.getElementById('firstName').classList.remove('invalid');
+      }
+    
+      if (!nameRegex.test(lastName.value)) {
+        document.getElementById('lastName').classList.add('invalid');
+        return
+      } else {
+        document.getElementById('lastName').classList.remove('invalid');
+      }
+    
+      if (!ageRegex.test(age.value) || age < 0) {
+        document.getElementById('age').classList.add('invalid');
+        return
+      } else {
+        document.getElementById('age').classList.remove('invalid');
+      }
+      if(nameRegex.test(firstName.value)||nameRegex.test(lastName.value||(ageRegex.test(age.value) || age < 0))){
+        document.getElementById('demo').innerHTML = `first name: ${firstName.value} <br> last: ${lastName.value}<br>age: ${age.value}`;
+      }
+     
+}
+
+document.getElementById('submit4').addEventListener('click',validateForm)
 // /Task 4
 
-// Task 5
-const input1 = document.getElementById('itemTask5.1');
-const input2 = document.getElementById('itemTask5.2');
-const input3 = document.getElementById('itemTask5.3');
-document.querySelectorAll('.input__task5').forEach(item => {
-    item.addEventListener('blur', (event) => {
-        document.getElementById('demo').textContent = `Input 1: ${input1.value}, Input 2: ${input2.value}, Input 3: ${input3.value}`;
-    });
-})
-// /Task 5
-
-// Task 6
-document.querySelectorAll('#itemTask6').forEach(item => {
-    item.addEventListener('click', () => {
-        alert(item.value)
-    }, { once: true })
-});
-// /Task 6
-
-// Task 7
-document.querySelectorAll('#itemTask7').forEach(item => {
-    item.addEventListener('click', () => {
-        const digitMatch = item.textContent.match(/\d+/);
-        const originalNumber = parseInt(digitMatch[0]);
-        console.log(originalNumber)
-        const squaredNumber = originalNumber * originalNumber;
-        const outputString = item.textContent.replace(originalNumber, squaredNumber);
-        item.textContent = outputString
-    })
-});
-// /Task 7
-
-// Task 8
-document.querySelectorAll('#itemTask8').forEach(item => {
-    item.addEventListener('blur', () => {
-        const correctLength = item.dataset.length;
-        const inputDataLength = item.value.length;
-        if (correctLength == inputDataLength) {
-            item.style.border = '3px solid green';
-
-        } else {
-            item.style.borderColor = 'red';
-        }
-
-    })
-});
-// /Task 8
-
 // Task 2.1
+let counter = 1
 document.querySelectorAll('#itemTask2_1').forEach(item => {
     item.addEventListener('click', () => {
         const styles = window.getComputedStyle(item);
         const backgroundColor = styles.backgroundColor;
-        console.log(backgroundColor)
-        if (backgroundColor == 'rgb(0, 128, 0)') {
-            return item.style.backgroundColor = 'red';
+        const colorList = ['green','red','yellow']
+        const textList = ['Update','Delete','Pendding']
+        if(counter == 1){
+             item.style.backgroundColor = colorList[0];
+             item.textContent = textList[0]
+            counter++
+            return
         }
-        return item.style.backgroundColor = 'green';
+        if(counter == 2){
+            item.style.backgroundColor = colorList[1];
+            item.textContent = textList[1]
+           counter++
+           return
+       }
+       if(counter == 3){
+        item.style.backgroundColor = colorList[2];
+        item.textContent = textList[2]
+       counter = 1
+        }
 
     })
 });
@@ -126,39 +120,6 @@ const generateChessBoard = () => {
     document.body.appendChild(chessBoard);
 }
 
-const generateChessBoardPlus = () => {
-    const chessBoard = document.createElement('div');
-    chessBoard.classList.add('chess-board');
-
-    const piecesOrder = ['R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'];
-
-    for (let row = 8; row >= 1; row--) {
-        const rowDiv = document.createElement('div');
-        rowDiv.classList.add(row % 2 === 0 ? 'even-row' : 'odd-row');
-
-        for (let col = 1; col <= 8; col++) {
-            const cell = document.createElement('div');
-            cell.classList.add('cell');
-            const piece = document.createElement('div');
-            piece.classList.add('piece');
-
-            if (row === 1 || row === 8) {
-                piece.textContent = piecesOrder[col - 1];
-                piece.style.color = row === 1 ? 'black' : 'white';
-            } else if (row === 2 || row === 7) {
-                piece.textContent = 'P';
-                piece.style.color = row === 2 ? 'black' : 'white';
-            }
-
-            cell.appendChild(piece);
-            rowDiv.appendChild(cell);
-        }
-
-        chessBoard.appendChild(rowDiv);
-    }
-
-    document.body.appendChild(chessBoard);
-}
 
 const generateChessBoardPhoto = () => {
     const chessBoard = document.createElement('div');
@@ -198,5 +159,4 @@ const generateChessBoardPhoto = () => {
 
 
 generateChessBoard();
-generateChessBoardPlus();
 generateChessBoardPhoto();
