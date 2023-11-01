0b1a7d15d7ad1a64ef83c6dd06622f7f5402c0aa
igor203-grup <126685862+igor203-grup@users.noreply.github.com>
2023-11-01 17:21:54 +0200

    ordenko_203 fixed

---
+++ task_1/index.js
@@ -0,0 +1,61 @@
+function validateInput(input) {
+    const value = input.value.trim();
+    const id = input.id;
+
+    if (id === "firstName" || id === "lastName") {
+       
+        const capitalizedValue = value.charAt(0).toUpperCase() + value.slice(1);
+
+        if (!/^[A-Za-zА-Яа-яЁёІіЇїЄєҐґ]+$/.test(capitalizedValue)) {
+            input.classList.add("invalid-input");
+            return false;
+        }
+
+       
+        input.value = capitalizedValue;
+    } else if (id === "age") {
+        if (!/^\d+$/.test(value) || parseInt(value) > 150) {
+            input.classList.add("invalid-input");
+            return false;
+        }
+    }
+
+    input.classList.remove("invalid-input");
+    return true;
+}
+
+
+function validateForm() {
+    const firstNameInput = document.getElementById("firstName");
+    const lastNameInput = document.getElementById("lastName");
+    const ageInput = document.getElementById("age");
+    const demoMessage = document.getElementById("demo");
+
+    const isFirstNameValid = validateInput(firstNameInput);
+    const isLastNameValid = validateInput(lastNameInput);
+    const isAgeValid = validateInput(ageInput);
+
+    if (isFirstNameValid && isLastNameValid && isAgeValid) {
+        demoMessage.textContent = "Форма дійсна. Подання успішне!";
+        demoMessage.classList.remove("invalid-message");
+    } else {
+        demoMessage.textContent = "Форма недійсна. Перевірте введені дані.";
+        demoMessage.classList.add("invalid-message");
+    }
+    function rotateImage(event) {
+        if (event.button === 2) {
+            const img = event.target;
+            img.style.transform = `rotate(${(img.dataset.rotation || 0) + 90}deg)`;
+            img.dataset.rotation = (img.dataset.rotation || 0) + 90;
+        }
+    }
+    
+    const images = document.querySelectorAll(".image-container img");
+    
+    images.forEach((img) => {
+        img.addEventListener("contextmenu", (event) => event.preventDefault());
+        img.addEventListener("mousedown", rotateImage);
+        
+    });
+    
+}
