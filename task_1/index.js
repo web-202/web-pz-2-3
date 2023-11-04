838609d436b9753dcd4d9bb47828d22fac23c165
marina458 <marinaandru90@gmail.com>
2023-11-04 13:54:26 +0200

    Andrushenko_202(fix)

--- task_1/index.js
+++ task_1/index.js
@@ -1,39 +1,12 @@
-const images = document.querySelectorAll('.images img');
-images.forEach(img => {
-    img.addEventListener('click', () => {
-        const href = img.getAttribute('href');
-        alert(`Link: ${href}`);
-    });
-
-    img.addEventListener('contextmenu', e => {
-        e.preventDefault();
-        const currentRotation = img.style.transform.replace(/[^0-9-]/g, '') || 0;
-        const newRotation = (parseInt(currentRotation) + 90) % 360;
-        img.style.transform = `rotate(${newRotation}deg)`;
-    });
-});
-
-const links = document.querySelectorAll('.links a');
-links.forEach(link => {
-    let originalText = link.textContent;
-    link.addEventListener('mouseenter', () => {
-        originalText = link.textContent;
-        link.textContent = `${originalText} (${link.getAttribute('href')})`;
-    });
-    link.addEventListener('mouseleave', () => {
-        link.textContent = originalText;
-    });
-
-});
-
 const form = document.getElementById('form');
-const nameInput = document.getElementById('name');
-const secondNameInput = document.getElementById('secondName');
-const ageInput = document.getElementById('age');
 const demo = document.getElementById('demo');
 
 form.addEventListener('submit', function (e) {
     e.preventDefault();
+
+    const nameInput = document.getElementById('name');
+    const secondNameInput = document.getElementById('secondName');
+    const ageInput = document.getElementById('age');
 
     const name = nameInput.value.trim();
     const secondName = secondNameInput.value.trim();
@@ -44,13 +17,25 @@ form.addEventListener('submit', function (e) {
     const ageIsValid = /^\d+$/.test(age) && age >= 0;
 
     if (!nameIsValid) {
-        demo.textContent = 'Name is invalid.';
-        demo.style.color = 'red';
-    } else if (!secondNameIsValid) {
-        demo.textContent = 'Second Name is invalid.';
-        demo.style.color = 'red';
-    } else if (!ageIsValid) {
-        demo.textContent = 'Age is invalid.';
+        nameInput.style.borderColor = 'red';
+    } else {
+        nameInput.style.borderColor = '';
+    }
+
+    if (!secondNameIsValid) {
+        secondNameInput.style.borderColor = 'red';
+    } else {
+        secondNameInput.style.borderColor = '';
+    }
+
+    if (!ageIsValid) {
+        ageInput.style.borderColor = 'red';
+    } else {
+        ageInput.style.borderColor = '';
+    }
+
+    if (!nameIsValid || !secondNameIsValid || !ageIsValid) {
+        demo.textContent = 'Form is invalid.';
         demo.style.color = 'red';
     } else {
         demo.textContent = 'Form is valid.';
