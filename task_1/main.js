524e02f21e3617d7e0ca109f761a64186ce7f294
BogdanPalukh <palukh.bogdan1337@gmail.com>
2023-11-11 14:11:32 +0300

    fix

--- task_1/main.js
+++ task_1/main.js
@@ -7,6 +7,7 @@ function alertHref(id){
 const imgWheel = document.querySelectorAll('.img-wheel')
 imgWheel.forEach(item => {
   item.addEventListener('contextmenu', () => {
+    event.preventDefault()
     item.style.transform = 'rotate(90deg)'
   });
 });
@@ -34,6 +35,7 @@ document.getElementById('submitButton').addEventListener('click', () => {
   const lastName = document.getElementById('lastName').value;
   const age = document.getElementById('age').value;
 
+
   const namePattern = /^[A-Za-z\s]{1,50}$/;
   const agePattern = /^[0-9]+$/;
 
@@ -54,4 +56,15 @@ document.getElementById('submitButton').addEventListener('click', () => {
   } else {
     document.getElementById('demo').textContent = 'Invalid';
   }
+  let resultDiv = document.getElementById("demo");
+  resultDiv.textContent = "Значення firstName: " + firstName + ", lastName: " + lastName + ", age: " + age;
 });
+
+function validateInput(input) {
+  let value = parseInt(input.value, 10);
+
+  if (isNaN(value) || value < 1 || value > 100) {
+    input.value = '';
+    alert('Введіть число в діапазоні від 1 до 100');
+  }
+}
