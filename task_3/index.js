9de13f5e2f1467eb8596b43d5a2bd2b2f8ba51c4
PolivanchukRostislav <92991025+PolivanchukRostislav@users.noreply.github.com>
2023-11-13 14:46:56 +0200

    Polivanchuk(202): fix task_3/index.js

--- task_3/index.js
+++ task_3/index.js
@@ -1,13 +1,13 @@
-const chessBoardBtn = document.querySelector(".prs-button");
-const chessBoardContainer = document.querySelector(".chess-board");
+const chessButton = document.querySelector(".prs-button");
+const chessContainer = document.querySelector(".chess-board");
 
 const generateChessBoard = () => {
-    const chessBoard = new ChessBoard(chessBoardContainer);
+    const chessBoard = new ChessBoard(chessContainer);
     chessBoard.drawBoard();
-    chessBoardBtn.style.display = "none"; 
+    chessButton.style.display = "none"; 
 };
 
-chessBoardBtn.addEventListener("click", generateChessBoard);
+chessButton.addEventListener("click", generateChessBoard);
 
 function ChessBoard(block) {
     this.size = 8;
@@ -21,7 +21,7 @@ function ChessBoard(block) {
         chessboardTable.appendChild(lettersRow);
 
         for (let row = 0; row < this.size; row++) {
-            const chessBoardRow = this.generateChessBoardRow(row);
+            const chessBoardRow = this.generateChessRow(row);
             chessboardTable.appendChild(chessBoardRow);
         }
 
@@ -32,7 +32,7 @@ function ChessBoard(block) {
         block.appendChild(chessboardTable);
     }
 
-    this.generateChessBoardRow = (row) => {
+    this.generateChessRow = (row) => {
         const chessBoardRow = document.createElement("tr");
         chessBoardRow.className = "chess-board_row";
 
@@ -41,7 +41,7 @@ function ChessBoard(block) {
         chessBoardRow.appendChild(numberCell);
 
         for (let column = 0; column < this.size; column++) {
-            chessBoardRow.appendChild(this.generateChessBoardCell(row, column));
+            chessBoardRow.appendChild(this.generateFigyre(row, column));
         }
 
         numberCell = this.generateNumberCell(this.size - row)
@@ -58,7 +58,7 @@ function ChessBoard(block) {
         return numberCell;
     };
 
-    this.generateChessBoardCell = (row, column) => {
+    this.generateFigyre = (row, column) => {
         const chessBoardCell = document.createElement("td");
         chessBoardCell.className = "chess-board_cell";
         chessBoardCell.classList.add((row + column) % 2 === 0 ?
@@ -67,34 +67,34 @@ function ChessBoard(block) {
         switch (row) {
             case 0:
                 if (column === 0 || column === 7) {
-                    chessBoardCell.appendChild(this.getChessPiecesImg("rook"));
+                    chessBoardCell.appendChild(this.getChessImg("rook"));
                 } else if (column === 1 || column === 6) {
-                    chessBoardCell.appendChild(this.getChessPiecesImg("knight"));
+                    chessBoardCell.appendChild(this.getChessImg("knight"));
                 } else if (column === 2 || column === 5) {
-                    chessBoardCell.appendChild(this.getChessPiecesImg("bishop"));
+                    chessBoardCell.appendChild(this.getChessImg("bishop"));
                 } else if (column === 3) {
-                    chessBoardCell.appendChild(this.getChessPiecesImg("queen"));
+                    chessBoardCell.appendChild(this.getChessImg("queen"));
                 } else if (column === 4) {
-                    chessBoardCell.appendChild(this.getChessPiecesImg("king"));
+                    chessBoardCell.appendChild(this.getChessImg("king"));
                 }
                 break;
             case 1:
-                chessBoardCell.appendChild(this.getChessPiecesImg("pawn"));
+                chessBoardCell.appendChild(this.getChessImg("pawn"));
                 break;
             case 6:
-                chessBoardCell.appendChild(this.getChessPiecesImg("pawnw"));
+                chessBoardCell.appendChild(this.getChessImg("pawnw"));
                 break;
             case 7:
                 if (column === 0 || column === 7) {
-                    chessBoardCell.appendChild(this.getChessPiecesImg("rookw"));
+                    chessBoardCell.appendChild(this.getChessImg("rookw"));
                 } else if (column === 1 || column === 6) {
-                    chessBoardCell.appendChild(this.getChessPiecesImg("knightw"));
+                    chessBoardCell.appendChild(this.getChessImg("knightw"));
                 } else if (column === 2 || column === 5) {
-                    chessBoardCell.appendChild(this.getChessPiecesImg("bishopw"));
+                    chessBoardCell.appendChild(this.getChessImg("bishopw"));
                 } else if (column === 3) {
-                    chessBoardCell.appendChild(this.getChessPiecesImg("queenw"));
+                    chessBoardCell.appendChild(this.getChessImg("queenw"));
                 } else if (column === 4) {
-                    chessBoardCell.appendChild(this.getChessPiecesImg("kingw"));
+                    chessBoardCell.appendChild(this.getChessImg("kingw"));
                 }
                 break;
         }
@@ -102,7 +102,7 @@ function ChessBoard(block) {
         return chessBoardCell;
     }
 
-    this.getChessPiecesImg = (type) => {
+    this.getChessImg = (type) => {
         const chessPiecesImg = document.createElement("img");
         chessPiecesImg.className = "chess-pieces";
         chessPiecesImg.src = `./img/${type}.png`;
