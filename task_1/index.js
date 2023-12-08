217508571725f9160589c1082ef55c598b71ad3a
MihailM12 <mihail.melnik.44@gmail.com>
2023-12-08 15:59:09 +0200

    1.0.0

---
+++ task_1/index.js
@@ -0,0 +1,50 @@
+const specialChars = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+function rotate(element){
+    element.style.transform = "rotate(90deg)";
+}
+function alertAction(element){
+    alert("click on images")
+}
+function addHref(link) {
+    const text = link.textContent;
+    if (text.includes('(') && text.includes(')')) {
+        const textInParentheses = text.match(/\(([^)]+)\)/)[1];
+        link.href = textInParentheses;
+    }
+}
+
+function removeHref(link) {
+    link.removeAttribute('href');
+}
+
+function submitForm() {
+    const form = document.getElementById('userForm');
+    const demo = document.getElementById('demo');
+    if (form.checkValidity()) {
+        const firstName = document.getElementById('firstName').value;
+        const lastName = document.getElementById('lastName').value;
+        const age = document.getElementById('age').value;
+        if (specialChars.test(firstName) || specialChars.test(lastName) || firstName.length > 50 || lastName.length > 50) {
+            alert("Ім'я та прізвище не повинні містити спеціальних символів і не можуть бути довшими за 50 символів");
+            form.reset();
+        }
+        else if(age >= 100 ){
+            alert("age must be < 100")
+            form.reset();
+        }else{
+        demo.innerHTML = `Дані надіслані: Ім'я: ${firstName}, Прізвище: ${lastName}, Вік: ${age}`;
+        demo.style.display = 'block';
+        form.reset();
+        }
+    } else {
+        demo.style.display = 'none';
+    }
+}
+function disablecontext(e) {
+    var clickedEl = (e == null) ? event.srcElement.tagName : e.target.tagName;
+    if (clickedEl == "IMG") {
+        e.preventDefault();
+    }
+}
+
+document.oncontextmenu = disablecontext;
